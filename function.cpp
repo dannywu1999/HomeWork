d70f6c1ecb87f9db786b1af0fa382ab8a92fd1dc
dannywu1999 <dannywu199988@gmail.com>
2017-05-23 20:42:26 +0800

    加減乘除

---
+++ function.cpp
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int add(int A, int B){
+	int result = 0;
+	
+	result = A + B;
+	
+	return result;	
+}
+int sub(int C, int D)
+{
+	int result = 0;
+	
+	result = C - D;
+	
+	return result;
+}
+int mul(int E, int F)
+{
+	int result = 0;
+	
+	result = E * F;
+	
+	return result;
+}
+int divide(int G, int H)
+{
+	int result = 0;
+	
+	if(H != 0)
+	{	
+		result = G / H;
+	}
+	else
+	{
+		printf("error ");
+	}
+	
+	return result;
+}
+int main(){
+	
+	int result = 0,A,B;
+	A = 8;
+	B = 0;
+	printf("result:A+B=%d\n", add(A, B));
+	
+	result = add(A, B);
+	printf("result:A+B=%d\n", result);	
+	printf("result:A-B=%d\n", sub(A,B));
+	
+	result = sub( A, B);
+	printf("result:A-B=%d\n",result);
+	printf("result:A*B=%d\n", mul(A,B));
+	result = mul(A, B);
+	printf("result:A*B=%d\n",result);
+	result = divide(A,B);
+	printf("result:A/B=%d\n",result);
+	printf("result:A/B=%d\n", divide(A, B));
+}
