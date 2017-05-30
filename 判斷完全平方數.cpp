645530e11a996fee525c17a8e5ec7b74c8068799
dannywu1999 <dannywu199988@gmail.com>
2017-05-30 21:03:36 +0800

    identify a number is 完全平方數 using function

--- 判斷完全平方數.cpp
+++ 判斷完全平方數.cpp
@@ -1,56 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <iostream>
+//#include <iostream>
 
 bool isSquare(int N)
 {
 	bool result = false;
-	for(int i=1; i * i <= N ;i++)
+	for(int i = 1 ; i * i <= N ;i++)
 	{
-		if(N==i*i)
+		if( N == i * i)
 		{
 			return true;
 		}
 	}
 	return false;
 }
-
-
-/*
-bool isPrine(int N)
-{
-	bool result = false;
-	if(N==2){
-		return true;
-	}
-	if(N%2==0){
-		return false;	
-	}
-	for(int i=3; i * i <= N ;i+=2 )
-	{
-		//printf("%d\n",i);
-		if(N%i==0)
-		{
-			return false;
-		}
-	}
-	return true;
-}
-
-int main()//prine½è¼Æ 
-{	
-	int N;
-	scanf("%d",&N);
-	
-	if(isPrine(N)){
-		printf("%d is prime\n", N);	
-	}
-	else{
-		printf("%d is NOT prime\n", N);	
-	}
-	
-	//printf("%d is%s prime\n", N, isPrine(N) ? "" : "NOT");
-}*/
 int main()
 {
 	int N;
